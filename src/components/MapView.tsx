a6eb3949c11751cd01e80607c8a92b624ca7a99f
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-05 14:26:57 -0600

    Feat. Display Mapbox map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,18 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useRef, useLayoutEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import { PlacesContext } from '../context';
 import { Loading } from './Loading';
-
 
 export const MapView = () => {
 
     const { isLoading, userLocation } = useContext(PlacesContext);
+    const mapDiv = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
+        if (!isLoading) {
+            const map = new mapboxgl.Map({
+                container: mapDiv.current!,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: userLocation,
+                zoom: 15,
+            });
+        }
+    }, [isLoading])
 
     if (isLoading) {
         return (<Loading />)
     }
 
     return (
-        <div>
+        <div ref={mapDiv}
+            style={{
+                height: '100vh',
+                left: 0,
+                position: 'fixed',
+                top: 0,
+                width: '100vw',
+            }}
+        >
 
             {userLocation?.join(',')}
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+mapboxgl.accessToken = 'pk.eyJ1IjoibWNtbWlndWVsIiwiYSI6ImNsb2x3aWR0ZzBteXAyaW9hM3FtbGw4NGQifQ.SKUi1VvVcIXsob4PpFtz_g';
+
 if (!navigator.geolocation) {
   alert('Tu navegador no está habilitado para geolocalización');
   throw new Error('Tu navegador no está habilitado para geolocalización');
