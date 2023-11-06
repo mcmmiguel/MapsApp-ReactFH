5a4997a1038d7502c8c36027b4c686e36eceef86
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-05 20:15:46 -0600

    Fix. SearchResult height

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -35,31 +35,34 @@ export const SearchResults = () => {
     if (places.length === 0) return <></>
 
     return (
-        <ul className="list-group mt-3">
-            {
-                places.map((place) => (
-                    <li
-                        key={place.id}
-                        className={`list-group-item list-group-item-action pointer ${(activeId === place.id) ? 'active' : ''}`}
-                        onClick={() => onPlaceClicked(place)}
-                    >
-                        <h6>{place.text_es}</h6>
-                        <p
-                            style={{ fontSize: '12px' }}
+        <div className='container'>
+
+            <ul className="list-group mt-3 overflow-auto" style={{ maxHeight: '500px' }}>
+                {
+                    places.map((place) => (
+                        <li
+                            key={place.id}
+                            className={`list-group-item list-group-item-action pointer ${(activeId === place.id) ? 'active' : ''}`}
+                            onClick={() => onPlaceClicked(place)}
                         >
-                            {place.place_name_es}
-                        </p>
-                        <div>
-                            <button
-                                className={`btn btn-sm ${(activeId === place.id) ? 'btn-outline-light' : 'btn-outline-primary'}`}
-                                onClick={() => getRoute(place)}
+                            <h6>{place.text_es}</h6>
+                            <p
+                                style={{ fontSize: '12px' }}
                             >
-                                Direcciones
-                            </button>
-                        </div>
-                    </li>
-                ))
-            }
-        </ul>
+                                {place.place_name_es}
+                            </p>
+                            <div>
+                                <button
+                                    className={`btn btn-sm ${(activeId === place.id) ? 'btn-outline-light' : 'btn-outline-primary'}`}
+                                    onClick={() => getRoute(place)}
+                                >
+                                    Direcciones
+                                </button>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 };
